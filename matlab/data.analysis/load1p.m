1148915ba61bf70228ca52cb5dff04ccef32aa81
svn <svn@de0f5a65-c6c9-414c-ab6f-108cdf64cfb5>
2009-11-29 17:11:29 +0000

    modified load1p.m added argument for filename
    git-svn-id: http://10.10.170.94/program@2 de0f5a65-c6c9-414c-ab6f-108cdf64cfb5

--- matlab/data.analysis/load1p.m
+++ matlab/data.analysis/load1p.m
@@ -1,28 +1,43 @@
-function data=load1p(fileno)
+function data = load1p(FullFileName)
+% load1p: Load one multi-page TIFF image into a matrix.
+%         The matrix is (height, width, number of pages).
+% Syntax:
+%   a = load1p(FullFileName)
+%   FullFileName is a string and should be filename with full path, 
+%   e.g. 'C:\data\imaging.tif' .
 
-[FileName,PathName] = uigetfile('*.tif','Select the TIF file','H:\');
-path=strcat(PathName,FileName);
+% Written by Li Hao @ Guo Lab, ION, in 2009.
+% Modified by Zhou Bangyu @ Guo Lab, ION, Nov. 29, 2009.
 
 if (nargin == 0)
-    INFO=imfinfo(path);
-    j=numel(INFO);
-    x=INFO(1).Width;
-    y=INFO(1).Height;
+    [FileName, PathName] = uigetfile('*.tif','Select the TIFF file','H:\');
+    if isequal(FileName, 0)
+        disp('TIFF Loading Canceled');
+        return
+    end
+    FullFileName = strcat(PathName, FileName);
 else
-    example=imread(path,1);
-    [y x]=size(example);
-    j=fileno;
+    example = imread(FullFileName, 1);
+    [y x] = size(example);
+%     nPage=fileno;
 end
-data=zeros(y,x,j,'uint16');
 
-handle=waitbar(0,'Loading image');
-for i=1:j
+INFO = imfinfo(FullFileName);
+% number of pages in the multi-page TIFF
+nPage = numel(INFO);
+x = INFO(1).Width;
+y = INFO(1).Height;
+
+data = zeros(y, x, nPage, 'uint16');
+
+handle = waitbar(0,'Loading image');
+for i = 1:nPage
 %   Only after R2009a, elsewise use the following line
-%     data(:,:,i)=imread(path,i);
-    data(:,:,i)=imread(path,i,'Info',INFO);
-    waitbar(i/j,handle);
+%     data(:,:,i) = imread(FullFileName,i);
+    data(:,:,i) = imread(FullFileName, i, 'Info', INFO);
+    waitbar(i/nPage, handle);
 end
-close(handle);
-disp(path);
+delete(handle);
+disp(FullFileName);
 
 return
