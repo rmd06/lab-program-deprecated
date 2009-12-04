07bd829f9dcbbe05202f906cde44c92f2f7f8a3c
svn <svn@de0f5a65-c6c9-414c-ab6f-108cdf64cfb5>
2009-12-04 20:03:14 +0000

    wrote some script, not yet complete
    git-svn-id: http://10.10.170.94/program@5 de0f5a65-c6c9-414c-ab6f-108cdf64cfb5

--- matlab/data.analysis/script.es.roi.m
+++ matlab/data.analysis/script.es.roi.m
@@ -1,5 +1,6 @@
 % Script for imaging processing
 
+%%
 % Load files, images
 imageFileName = '';
 timingFileName = '';
@@ -7,5 +8,24 @@ timingFileName = '';
 imageData = load1p(imageFileName);
 load timingFileName;  % arrary named data, and protocol, loaded
 
-% get ROIs and compute BOT
+%%
+% ROIs and compute BOT
+handleImage = imagesc(imageData(:, :, 1));
+
+prompt = {'Total ROI Number:'};  % ask for ROI numberdlg_title = 'Inputs for mmROI function';
+num_lines = 1;
+def = {'1'};
+inputs  = str2num(char(inputdlg(prompt, dlg_title, num_lines, def)));
+nRoi = inputs(1);  % get ROI number
+
+roiInfo = getroi(nRoi, handleImage);
+botData = bot(imageData, roiInfo);
+% todo: save the ROI position figure
+
+%%
+% Find timing infomation
+[tFrameUp tEsUp tFrameOnEsUp tFrameOnEsDown] = timing(data);
+
+botPlotData = (tFrameUp, botData);
+
 
