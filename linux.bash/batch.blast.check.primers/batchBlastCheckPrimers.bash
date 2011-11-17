5bd0bde539eea0eb7b783f38f40658dc791b6982
svn <svn@de0f5a65-c6c9-414c-ab6f-108cdf64cfb5>
2011-11-17 01:24:39 +0000

    git-svn-id: http://10.10.170.94/program@27 de0f5a65-c6c9-414c-ab6f-108cdf64cfb5

---
+++ linux.bash/batch.blast.check.primers/batchBlastCheckPrimers.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+DATE=`date +%d_%b_%y_%R`
+CD=`pwd`
+subject=/media/data/zby/document/gene.sequence/pUAST_8904.fa
+outfile=seq.result.htm
+FILES=*.seq
+
+echo \<html\> > $outfile
+echo \<head\> >> $outfile
+echo \<title\> Results $DATE \<\/title\> >> $outfile
+echo \<\/head\> >> $outfile
+echo \<body\> >> $outfile
+
+echo \<h1\> Results for files in $CD \<\/h1\> >> $outfile
+echo \<table border="0" \> >> $outfile
+
+shopt -s nullglob
+for f in $FILES
+do
+  echo "Processing $f file..."
+
+  # write table in html
+  echo \<tr\> >> $outfile
+  echo \<th\> >> $outfile
+  
+  #table header
+  echo $f >> $outfile
+  echo \<\/th\> >> $outfile
+  echo \<\/tr\> >> $outfile
+
+  echo \<tr\> >> $outfile
+  echo \<td style="background-color:#ccccff;"\> >> $outfile
+  echo \<pre\> >> $outfile
+  
+  # do the blast
+   blastn -task 'blastn-short' -query $f -subject $subject -outfmt 0 >> $outfile
+   
+  echo \<\/pre\> >> $outfile
+  echo \<\/td\> >> $outfile
+  echo \<\/tr\> >> $outfile
+
+done
+
+echo \<\/table\> >> $outfile
+echo \<\/body\> >> $outfile
+echo \<\/html\> >> $outfile
+
+
