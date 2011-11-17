b915b8f9716457095b76bbd65d226332e56fabda
svn <svn@de0f5a65-c6c9-414c-ab6f-108cdf64cfb5>
2011-11-17 01:13:09 +0000

    added batch script for checking prmiers
    git-svn-id: http://10.10.170.94/program@26 de0f5a65-c6c9-414c-ab6f-108cdf64cfb5

---
+++ win.bat/batchBlastCheckPrimers/batchBlastCheckPrimers.cmd
@@ -0,0 +1,50 @@
+:: ver. 1.01b, last update 2011 Oct 9
+:: author zby
+
+:: DESCRIPTION Aims to offer an easy way to check primers (short nucleotides)
+::             against one common template sequence.
+::             Each sequencing result file (with .seq) is checked using blastn as 
+::             "query", the template sequence as "subject".
+::             The outputs are collected and combined into one html file.
+
+:: REQUIREMENT This windows script requires a local installation of blast+ .
+::             Please refer to 
+::             http://www.ncbi.nlm.nih.gov/staff/tao/URLAPI/pc_setup.html
+::             for installation guides, and
+::             ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ 
+::             for appropriate files.
+
+:: USAGE 1. Copy batchBlast.cmd (this file) and writeResults.cmd into 
+::              sequencing results folder.
+::       2. Set the full filename (with path) for the target or template 
+::              sequence file.
+::       3. Run this batch file, by double-clicking this file or run in a  
+::              command line interface.
+
+@echo off
+
+setlocal
+
+set subject="e:\zby\document\gene.sequence\pUAST_8904.fa"
+set outFile=result.htm
+
+forfiles /m *.seq /c "cmd /c blastn -task blastn-short -query @file -subject %subject% -outfmt 0 > @fname.blastout"
+
+echo ^<html^> > %outFile%
+echo ^<head^> >> %outFile%
+echo ^<title^> Results %DATE% %TIME% ^<^/title^> >> %outFile%
+echo ^<^/head^> >> %outFile%
+echo ^<body^> >> %outFile%
+
+echo ^<h1^> Results for files in %CD% ^<^/h1^> >> %outFile%
+echo ^<table border="0" ^> >> %outFile%
+
+for /f %%f in ('dir /b *.blastout') do call writeResults.cmd %%f %outFile%
+
+echo ^<^/table^> >> %outFile%
+echo ^<^/body^> >> %outFile%
+echo ^<^/html^> >> %outFile%
+
+for /f %%f in ('dir /b *.blastout') do del %%f
+
+endlocal
