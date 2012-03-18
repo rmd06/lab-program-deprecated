08a8396825a4995e642c88e114bbc9e061b04df6
svn <svn@de0f5a65-c6c9-414c-ab6f-108cdf64cfb5>
2012-03-18 18:11:55 +0000

    git-svn-id: http://10.10.170.94/program@40 de0f5a65-c6c9-414c-ab6f-108cdf64cfb5

--- win.bat/batch.ffmpeg/transcodeForAllSubDir.bat
+++ win.bat/batch.ffmpeg/transcodeForAllSubDir.bat
@@ -3,6 +3,19 @@
 REM Reference: http://stackoverflow.com/questions/8397674/windows-batch-file-looping-through-directories-to-process-files
 
 REM REQUIRE ffmpeg installation
+echo.
+echo ========================================
+echo About to convert ALL .avi files to .mp4
+echo   including ALL subdirectories!
+echo If this is NOT you what want, 
+echo   CLOSE THIS WINDOW to end this batch.
+echo   -----------------
+echo If this is exactly you want,
+echo          -- press any key to continue.
+echo ========================================
+echo.
+
+pause
 
 call :treeProcess
 goto :eof
@@ -10,8 +23,10 @@ goto :eof
 :treeProcess
 REM Do whatever you want here over the files of this subdir, for example:
 for %%f in (*.avi) do (
+    if exist new-%%~nf.mp4 goto :eof
     ffmpeg -i %%f -vcodec mpeg4 new-%%~nf.mp4
 )
+
 for /D %%d in (*) do (
     cd %%d
     call :treeProcess
