eb27e3fecd6ee56132b9645edf0f2158f221829f
svn <svn@de0f5a65-c6c9-414c-ab6f-108cdf64cfb5>
2012-04-23 04:52:50 +0000

    git-svn-id: http://10.10.170.94/program@59 de0f5a65-c6c9-414c-ab6f-108cdf64cfb5

--- R.code/courtship.srt.csv/summaryForCourtship.R
+++ R.code/courtship.srt.csv/summaryForCourtship.R
@@ -142,8 +142,10 @@ sumCourtshipDir <- function(csvDir="", out=TRUE, outfile=paste(csvDir, "/summary
     }
     
     if (na.zero)
+    {
         print("All NAs converted to 0.")
         return(sumCsvNoNA)
+    }
     else
         return(sumCsv)
 }
